fe7ffd118fb314ad89d5454aea3d3f7ed5bc0233
projectormato <k.keita811.anuy@gmail.com>
2017-11-30 16:13:51 +0900

    add Projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,13 @@
+class ProjectsController < ApplicationController
+  def tasks
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/projects', to: 'projects#index'
+  get 'projects/show'
+  get 'projects/tasks'
+  get 'projects/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
